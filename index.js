da4a0e5449d4ececd381626a7942662ba39e8b8f
Kaan Yurdakoş <kaanyurdakosx@gmail.com>
2024-07-11 20:28:00 +0300

    Add MongoDB connection

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const usersRouter = require('./routes/users')
+const tweetsRouter = require('./routes/tweets')
 const indexRouter = require('./routes/index')
+require('./mongo-connection')
 
 const app = express()
 app.use(bodyParser.json())
@@ -9,6 +11,7 @@ app.use(bodyParser.json())
 app.set('view engine', 'pug')
 
 app.use('/users', usersRouter)
+app.use('/tweets', tweetsRouter)
 app.use('/', indexRouter)
 
 app.listen(3001, () => {
